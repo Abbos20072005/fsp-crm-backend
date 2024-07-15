126b66f284d02093f3445f38cbaf30db5d385344
MukhammadAli <rasulovadildora3@gmail.com>
2024-07-15 09:16:00 +0500

    added config urls, auth model added kpi fixed salary

--- authentication/models.py
+++ authentication/models.py
@@ -12,6 +12,8 @@ ROLE_CHOICES = (
 class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
     role = models.IntegerField(choices=ROLE_CHOICES, default=1)
+    kpi = models.DecimalField(default=0)
+    fixed_salary = models.DecimalField(default=0)
 
     def __str__(self):
         return self.username
