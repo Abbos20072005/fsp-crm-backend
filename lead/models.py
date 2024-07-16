56bd93894238567196bfd8fd92cd4d17eade2497
Akbar <148070512+Akbar297@users.noreply.github.com>
2024-07-16 18:27:46 +0500

    added model

--- lead/models.py
+++ lead/models.py
@@ -1,27 +1,37 @@
 from django.db import models
+from authentication.models import User
 
-STATUS_CHOICES = (
-    (1, 'JOINED'),
-    (2, 'INTERESTED'),
-    (3, 'PAYED'),
-    (4, 'CANCELLED')
-)
+
+class Comment(models.Model):
+    comment = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
 
 
 class Lead(models.Model):
+    STATUS_CHOICES = (
+        (1, 'JOINED'),
+        (2, 'INTERESTED'),
+        (3, 'PAYED'),
+        (4, 'CANCELLED'),
+    )
+
     name = models.CharField(max_length=250)
     phone = models.CharField(max_length=100)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=2)
     address = models.CharField(max_length=100)
-    admin = models.ForeignKey()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-
-
-class Comment(models.Model):
-    pass
 
 
 class Student(models.Model):
@@ -31,12 +41,12 @@ class Student(models.Model):
     phone = models.CharField(max_length=100)
     passport_number = models.CharField(max_length=100)
     personal_number = models.CharField(max_length=100)
-    passport = models.CharField(max_length=100)
-    father_passport = models.CharField(max_length=100)
-    mother_passport = models.CharField(max_length=100)
-    ...
+    passport = models.FileField(upload_to='passport/')
+    father_passport = models.FileField(max_length=100)
+    mother_passport = models.FileField(max_length=100)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-
-
-
+    def __str__(self):
+        return self.full_name
