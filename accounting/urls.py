172f65da1d01c12b0899203f75727e353d004045
menotabek <baqara163@gmail.com>
2024-07-17 12:30:25 +0500

    delete and list url mixed

--- accounting/urls.py
+++ accounting/urls.py
@@ -4,16 +4,13 @@ from .views import CheckViewSet, OutcomeTypeViewSet, OutcomeViewSet
 urlpatterns = [
     path('checks/<int:pk>/', CheckViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'})),
     path('student/<int:pk>/checks/', CheckViewSet.as_view({'get': 'student_checks'})),
-    path('checks/create/', CheckViewSet.as_view({'post': 'create'})),
-    path('checks/', CheckViewSet.as_view({'get': 'list'})),
+    path('checks/', CheckViewSet.as_view({'get': 'list', 'post': 'create'})),
 
     path('outcometype/<int:pk>/', OutcomeTypeViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('outcometype/create/', OutcomeTypeViewSet.as_view({'post': 'create'})),
-    path('outcometypes/', OutcomeTypeViewSet.as_view({'get': 'list'})),
+    path('outcometypes/', OutcomeTypeViewSet.as_view({'get': 'list', 'post': 'create'})),
 
     path('outcome/<int:pk>/', OutcomeViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('outcome/create/', OutcomeViewSet.as_view({'post': 'create'})),
-    path('outcomes/', OutcomeViewSet.as_view({'get': 'list'})),
+    path('outcomes/', OutcomeViewSet.as_view({'get': 'list', 'post': 'create'})),
 
 ]
 
