30bb296afec5e0f581419d427a94daf5e49d1177
Akbar <148070512+Akbar297@users.noreply.github.com>
2024-07-23 09:16:23 +0500

    added filter from lead

--- lead/urls.py
+++ lead/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import LeadViewSet, FilteredLeadViewSet
+from .views import LeadViewSet, LeadListViewSet, FilteredLeadViewSet
 
 urlpatterns = [
     path('create/', LeadViewSet.as_view({'post': 'create'})),
     path('<int:lead_id>/', LeadViewSet.as_view({'put': 'update', 'delete': 'delete'})),
-    path('filter/', FilteredLeadViewSet.as_view({'get': 'list'})),
-    path('filter/status/', FilteredLeadViewSet.as_view({'post': 'check_status'})),
+    path('list/leads', LeadListViewSet.as_view({'get': 'list'})),
+
+    path('filter/', FilteredLeadViewSet.as_view({'post': 'check_status', 'get': 'filter_lead'})),
+
 ]
 
 """
--- lead/views.py
+++ lead/views.py
@@ -1,13 +1,13 @@
 from drf_yasg.utils import swagger_auto_schema
-
+from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
-
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Lead
 from .permissions import check_role
-from .serializer import LeadCreateSerializer, LeadUpdateSerializer, LeadSerializer, CommentSerializer, \
-    LeadStatusSerializer
+from .serializer import LeadCreateSerializer, LeadUpdateSerializer, LeadSerializer, LeadStatusSerializer, \
+    LeadFilterSerializer
 
 
 class LeadViewSet(ViewSet):
@@ -57,7 +57,7 @@ class LeadViewSet(ViewSet):
         return Response(data={"message": "Lead successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
-class FilteredLeadViewSet(ViewSet):
+class LeadListViewSet(ViewSet):
     @swagger_auto_schema(
         operation_description='Filter a Lead',
         responses={200: 'Lead filtered'},
@@ -68,6 +68,14 @@ class FilteredLeadViewSet(ViewSet):
         serializer = LeadSerializer(leads, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class FilteredLeadViewSet(ViewSet):
+    @swagger_auto_schema(
+        operation_description='Status lead',
+        responses={200: 'Lead status lead',
+                   404: 'Status not found'},
+        request_body=LeadStatusSerializer,
+    )
     def check_status(self, request):
         data = request.data
         serializer = LeadStatusSerializer(data=data)
@@ -83,4 +91,24 @@ class FilteredLeadViewSet(ViewSet):
             return Response(data={'error': 'Status not found'},
                             status=status.HTTP_404_NOT_FOUND)
         serializer = LeadSerializer(leads, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    queryset = Lead.objects.all()
+    serializer_class = LeadFilterSerializer
+
+    @swagger_auto_schema(
+        operation_description='Filter a Lead',
+        responses={200: 'Lead filtered'},
+    )
+    @action(detail=False, methods=['get'])
+    def filter_lead(self, request):
+        name = request.query_params.get('name')
+        phone = request.query_params.get('phone')
+        queryset = self.queryset
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if phone:
+            queryset = queryset.filter(phone__icontains=phone)
+
+        serializer = LeadFilterSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
