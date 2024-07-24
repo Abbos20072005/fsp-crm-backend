89dc7cd9b6a574e32730d4bf1fbb2e4145a0d3cb
MukhammadAli <rasulovadildora3@gmail.com>
2024-07-24 08:50:35 +0500

    added 401 response to Base model

--- core/BasePermissions.py
+++ core/BasePermissions.py
@@ -4,7 +4,10 @@ from rest_framework.response import Response
 
 def is_super_admin(func):
     def wrapper(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role == 4:
+        if not request.user.is_authenticated:
+            return Response(data={'error': 'Authentication credentials were not provided'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        elif request.user.role == 4:
             return func(self, request, *args, **kwargs)
 
         return Response(data={"error": "You do not have permission to perform this action"},
@@ -15,7 +18,10 @@ def is_super_admin(func):
 
 def is_super_admin_or_hr(func):
     def wrapper(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role in [3, 4]:
+        if not request.user.is_authenticated:
+            return Response(data={'error': 'Authentication credentials were not provided'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        elif request.user.role in [3, 4]:
             return func(self, request, *args, **kwargs)
         return Response(data={"error": "You do not have permission to perform this action"},
                         status=status.HTTP_403_FORBIDDEN)
@@ -25,7 +31,10 @@ def is_super_admin_or_hr(func):
 
 def is_accountant_or_super_admin(func):
     def wrapper(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role in [2, 4]:
+        if not request.user.is_authenticated:
+            return Response(data={'error': 'Authentication credentials were not provided'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        elif request.user.role in [2, 4]:
             return func(self, request, *args, **kwargs)
         return Response(data={"error": "You do not have permission to perform this action"},
                         status=status.HTTP_403_FORBIDDEN)
@@ -35,7 +44,10 @@ def is_accountant_or_super_admin(func):
 
 def is_employee(func):
     def wrapper(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role in [1, 2, 3, 4]:
+        if not request.user.is_authenticated:
+            return Response(data={'error': 'Authentication credentials were not provided'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        elif request.user.role in [1, 2, 3, 4]:
             return func(self, request, *args, **kwargs)
         return Response(data={"error": "You do not have permission to perform this action"},
                         status=status.HTTP_403_FORBIDDEN)
@@ -45,7 +57,10 @@ def is_employee(func):
 
 def is_from_student_department(func):
     def wrapper(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role in [1, 3, 4]:
+        if not request.user.is_authenticated:
+            return Response(data={'error': 'Authentication credentials were not provided'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        elif request.user.role in [1, 3, 4]:
             return func(self, request, *args, **kwargs)
         return Response(data={'error': 'You do not have permission to perform this action'},
                         status=status.HTTP_403_FORBIDDEN)
@@ -55,7 +70,10 @@ def is_from_student_department(func):
 
 def is_from_accounting_department(func):
     def wrapper(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role in [2, 3, 4]:
+        if not request.user.is_authenticated:
+            return Response(data={'error': 'Authentication credentials were not provided'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        elif request.user.role in [2, 3, 4]:
             return func(self, request, *args, **kwargs)
         return Response(data={'error': 'You do not have permission to perform this action'},
                         status=status.HTTP_403_FORBIDDEN)
@@ -65,7 +83,10 @@ def is_from_accounting_department(func):
 
 def is_admin_or_super_admin(func):
     def wrapper(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role in [1, 4]:
+        if not request.user.is_authenticated:
+            return Response(data={'error': 'Authentication credentials were not provided'},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        elif request.user.role in [1, 4]:
             return func(self, request, *args, **kwargs)
         return Response(data={'error': 'You do not have permission to perform this action'},
                         status=status.HTTP_403_FORBIDDEN)
