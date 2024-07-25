46f4147ace071fba8c75e5cdd5f158df21c3ae47
bilol <bilolnorov7@gmail.com>
2024-07-25 18:53:36 +0500

    admin dash

--- lead/urls.py
+++ lead/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import LeadViewSet, FilteredLeadViewSet, MyLeadViewSet
+from .views import LeadViewSet, FilteredLeadViewSet, LeadStatsViewSet
 
 urlpatterns = [
     path('create/', LeadViewSet.as_view({'post': 'create'})),
     path('<int:lead_id>/', LeadViewSet.as_view({'put': 'update', 'delete': 'delete'})),
     path('filter/', FilteredLeadViewSet.as_view({'get': 'list'})),
     path('filter/status/', FilteredLeadViewSet.as_view({'post': 'check_status'})),
-    path('admin_dash/leads/', MyLeadViewSet.as_view({"get": "my_leads"})),
-    path('admin-dash/salary/', MyLeadViewSet.as_view({"get": "my_salary"})),
+    path('admin_dash/leads/', LeadStatsViewSet.as_view({"get": "my_leads"})),
+    path('admin-dash/salary/', LeadStatsViewSet.as_view({"get": "my_salary"})),
     path('<int:lead_id>/', LeadViewSet.as_view({'put': 'update', 'delete': 'soft_delete'})),
     path('search/', LeadViewSet.as_view({'get': 'search_lead'})),
 ]
--- lead/views.py
+++ lead/views.py
@@ -18,6 +18,7 @@ from .serializer import LeadCreateSerializer, LeadUpdateSerializer, LeadSerializ
 from django.db.models import Q, Sum
 from .serializer import LeadCreateSerializer, LeadUpdateSerializer, LeadSerializer, LeadFilterSerializer
 from accounting.models import Check, ExpenditureStaff
+from core.BasePermissions import is_super_admin_or_hr
 
 
 class LeadViewSet(ViewSet):
@@ -125,47 +126,7 @@ class LeadViewSet(ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class MyLeadViewSet(ViewSet):
-    @swagger_auto_schema(
-        operation_description="Admin Dashboard",
-        operation_summary="Admin Dashboard",
-        manual_parameters=[
-            openapi.Parameter('start_date', type=openapi.TYPE_STRING, in_=openapi.IN_QUERY),
-            openapi.Parameter('end_date', type=openapi.TYPE_STRING, in_=openapi.IN_QUERY),
-            openapi.Parameter('lead_status', type=openapi.TYPE_STRING, in_=openapi.IN_QUERY),
-        ],
-        responses={200: MyLeadSerializer()},
-        tags=['admin_dashboard']
-    )
-    def my_leads(self, request, *args, **kwargs):
-        user = request.user
-
-        if user.is_authenticated:
-            start_date = request.GET.get('start_date')
-            end_date = request.GET.get('end_date')
-            lead_status = request.GET.get('lead_status')
-            if start_date or end_date:
-                filter_query = Q(admin=user)
-
-                if start_date:
-                    filter_query &= Q(created_at__gte=start_date)
-
-                if end_date:
-                    filter_query &= Q(created_at__lte=end_date)
-
-                if lead_status:
-                    filter_query &= Q(status=lead_status)
-
-                my_leads = Lead.objects.filter(filter_query)
-
-            else:
-                my_leads = Lead.objects.filter(admin=user)
-
-            total = len(my_leads)
-            serializer = MyLeadSerializer(my_leads, many=True, context={"total": total})
-            return Response(serializer.data, status.HTTP_200_OK)
-        return Response({"error": "user is not authenticated"}, status.HTTP_401_UNAUTHORIZED)
-
+class LeadStatsViewSet(ViewSet):
     @swagger_auto_schema(
         operation_description="Admin Dashboard",
         operation_summary="Admin Dashboard",
@@ -178,15 +139,17 @@ class MyLeadViewSet(ViewSet):
     )
     def my_salary(self, request, *args, **kwargs):
         user = request.user
-        if user.is_authenticated:
-            salary_month = request.GET.get('salary_month')
-            salary_year = request.GET.get('salary_year')
-            if salary_year or salary_month:
-                data = self.salary_calculation(user.id, year=salary_year, month=salary_month)
-            else:
-                date = self.salary_calculation(user.id, year=datetime.year, month=datetime.month)
-            return Response(data, status.HTTP_200_OK)
-        return Response({"error": "user is not authenticated"}, status.HTTP_401_UNAUTHORIZED)
+        if not user.is_authenticated:
+            return Response({"error": "user is not authenticated"}, status.HTTP_401_UNAUTHORIZED)
+
+        salary_month = request.GET.get('salary_month')
+        salary_year = request.GET.get('salary_year')
+
+        if salary_year or salary_month:
+            data = self.salary_calculation(user.id, year=salary_year, month=salary_month)
+        else:
+            data = self.salary_calculation(user.id, year=datetime.year, month=datetime.month)
+        return Response(data, status.HTTP_200_OK)
 
     def salary_calculation(self, pk, year=None, month=None):
         admin = User.objects.filter(id=pk, is_deleted=False).first()
